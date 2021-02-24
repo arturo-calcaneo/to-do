e7df5c283edc1228d14f659e547c57fcad3b0331
Arturo Alejandro C <arthurogoldenon@gmail.com>
2021-02-24 14:29:53 -0600

    Fase 3 - Acabando detalles (iziToast, To-do)

--- js/todoContent.js
+++ js/todoContent.js
@@ -1,18 +1,22 @@
 
+const saveTodo= function(){
+    iziToast.success({
+        theme: 'dark',
+        //icon: null,
+        title: 'Guardado',
+        //message: 'La lista fue actualizada.',
+        //messageColor: '#ffffff',
+        //titleColor: '#ffffff',
+        //color: '#ffffff',
+        backgroundColor: 'rgba(37, 184, 37, 0.87)',
+        progressBar: false,
+        timeout: 1900
+    });
+};
+
 function todoContent(){
     // Si se presiona enter en algun input
-    allInputOnEnter(function(event){
-        //console.log(event.target.value);
-        iziToast.success({
-            icon: null,
-            title: 'Guardado',
-            message: 'La lista fue actualizada.',
-            messageColor: '#ffffff',
-            titleColor: '#ffffff',
-            color: '#ffffff',
-            backgroundColor: 'rgba(37, 184, 37, 0.87)',
-            progressBar: false,
-            timeout: 1850
-        })
-    });
+    allInputOnEnter(function(event){ saveTodo(); });
+    // Si se presiona el botón guardar todo
+    saveAllClick(function(event){ saveTodo(); });
 }
