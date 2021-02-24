d09b73570d60b83b1292c9c45605c5a144faaa96
Arturo Alejandro C <arthurogoldenon@gmail.com>
2021-02-24 14:17:40 -0600

    Fase 3 - Implementación de iziToast

--- js/todoContent.js
+++ js/todoContent.js
@@ -1,6 +1,18 @@
 
 function todoContent(){
+    // Si se presiona enter en algun input
     allInputOnEnter(function(event){
-        console.log(event.target.value);
+        //console.log(event.target.value);
+        iziToast.success({
+            icon: null,
+            title: 'Guardado',
+            message: 'La lista fue actualizada.',
+            messageColor: '#ffffff',
+            titleColor: '#ffffff',
+            color: '#ffffff',
+            backgroundColor: 'rgba(37, 184, 37, 0.87)',
+            progressBar: false,
+            timeout: 1850
+        })
     });
 }
